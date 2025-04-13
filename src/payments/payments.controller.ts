db09e4244ff9e70936c84826c456050cce5d54f5
AgnerVillaFabrega <adejesusvilla@unicesar.edu.co>
2025-04-13 03:33:43 -0500

    section-11

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { PaymentsService } from './payments.service';
 import { PaymentSessionDto } from './dto/payment-session.dto';
 import { Request, Response } from 'express';
@@ -7,30 +8,30 @@ import { Request, Response } from 'express';
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
-  @Post('create-payment-session')
-  createPaymentSession(@Body() paymentSession:PaymentSessionDto){
+  // @Post('create-payment-session')
+  @MessagePattern('create.payment.session')
+  createPaymentSession(@Payload() paymentSession: PaymentSessionDto) {
     return this.paymentsService.createPaymentSession(paymentSession);
   }
 
   @Get('success')
-  success(){
-    return{
+  success() {
+    return {
       ok: true,
-      message: 'Payment successful'
-    }
+      message: 'Payment successful',
+    };
   }
 
   @Get('cancelled')
-  cancelled(){
-    return{
+  cancelled() {
+    return {
       ok: false,
-      message: 'Payment cancelled'
-    }
+      message: 'Payment cancelled',
+    };
   }
 
   @Post('webhook')
-  async stripeWebhook(@Req() req: Request, @Res() res: Response){
+  async stripeWebhook(@Req() req: Request, @Res() res: Response) {
     return this.paymentsService.stripeWebhook(req, res);
   }
-
 }
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,15 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import { envs } from 'src/config';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { envs, NATS_SERVICE } from 'src/config';
 import Stripe from 'stripe';
 import { PaymentSessionDto } from './dto/payment-session.dto';
-import { Request, response, Response } from 'express';
+import { Request, Response } from 'express';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
+  private readonly logger = new Logger('PaymentsService');
+
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
-    const { currency, items, orderId} = paymentSessionDto;
+    const { currency, items, orderId } = paymentSessionDto;
 
     const lineItems = items.map((item) => {
       return {
@@ -37,15 +41,20 @@ export class PaymentsService {
       cancel_url: envs.stripeCalcelUrl,
     });
 
-    return session;
+    // return session;
+    return {
+      cancelUrl: session.cancel_url,
+      successUrl: session.success_url,
+      url: session.url,
+    };
   }
 
   async stripeWebhook(req: Request, res: Response) {
     const sig = req.headers['stripe-signature'];
-    
+
     let event: Stripe.Event;
     const endpointSecret = envs.stripeEndpointSecret;
-    
+
     if (!sig) {
       return res.status(400).send('Missing stripe-signature header');
     }
@@ -64,18 +73,20 @@ export class PaymentsService {
     switch (event.type) {
       case 'charge.succeeded':
         const chargeSucceeded = event.data.object;
-        //TODO: Llamar nuestro microservicio
-        console.log({
-          metadata: chargeSucceeded.metadata,
-          orderId: chargeSucceeded.metadata.orderId
-        });
+        const payload = {
+          stripePaymentId: chargeSucceeded.id,
+          orderId: chargeSucceeded.metadata.orderId,
+          receiptUrl: chargeSucceeded.receipt_url,
+        };
+        // this.logger.log({ payload });
+        this.client.emit('payment.succeeded', payload);
         break;
-    
+
       default:
         console.log(`Evento ${event.type} no crontolado`);
         break;
     }
-    
+
     return res.status(200).json({ sig });
   }
 }
